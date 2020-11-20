4b79719549c0451ab17c96d89351b30f59dc51ba
YOGESHWARAN R <yogeshin247@gmail.com>
2020-11-20 10:19:47 +0530

    script added

---
+++ README.md
@@ -0,0 +1,21 @@
+# MaskUrl
+
+This simple Python Script mask the Phishing url under the Normal url. It generate a hidden url for social engineering.
+
+## Installation
+
+```bash
+> git clone https://github.com/yogeshwaran01/maskurl
+> cd maskurl
+> python3 maskurl.py
+```
+
+![example](sample.png)
+
+## Legal Disclaimer
+
+Usage of MaskUrl for attacking targets without prior mutual consent is illegal. It's the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program
+
+## Credits
+
+This Scrpit is inspired from [jaykali's maskphish](https://github.com/jaykali/maskphish)
---
+++ maskurl.py
@@ -0,0 +1,49 @@
+"""
+Description:
+-----------
+    This Script Mask the url with given and Keyword
+
+Usage:
+-----
+    python3 maskurl.py 
+    
+"""
+
+from urllib.parse import urlparse
+
+from requests import post
+
+
+banner = r"""
+ __  __      _      ____    _  __  _   _   ____    _     
+|  \/  |    / \    / ___|  | |/ / | | | | |  _ \  | |    
+| |\/| |   / _ \   \___ \  | ' /  | | | | | |_) | | |    
+| |  | |  / ___ \   ___) | | . \  | |_| | |  _ <  | |___ 
+|_|  |_| /_/   \_\ |____/  |_|\_\  \___/  |_| \_\ |_____|
+                                                       
+"""
+
+
+def Shortner(big_url: str) -> str:
+    """
+    Function short the big urls to short
+    """
+    return post(f"https://da.gd/s/?url={big_url}").text
+
+
+def MaslUrl(target_url: str, mask_domain: str, keyword: str) -> str:
+    """
+    Function mask the url with given domain and keyword
+    """
+    url = Shortner(target_url)
+    return f"{mask_domain}-{keyword}@{urlparse(url).netloc + urlparse(url).path}"
+
+
+if __name__ == "__main__":
+    print(f"\033[91m {banner}\033[00m")
+    print("\n")
+    target = input("Enter the url (With http or https): ")
+    domain = input("Enter the domain name to mask url (With http or https): ")
+    keyword = input("Enter the keywords (use '-' instead of whitespace): ")
+    print("\n")
+    print(f"\033[91m {MaslUrl(target, domain, keyword)}\033[00m")
