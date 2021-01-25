53485339e7cd887b6547c5a62c2d8c15f64c94ad
YOGESHWARAN R <yogeshin247@gmail.com>
2021-01-25 21:09:41 +0530

    Added CLI utility

--- maskurl.py
+++ maskurl.py
@@ -1,7 +1,7 @@
 """
 Description:
 -----------
-    This Script Mask the url with given and Keyword
+    This Script Mask the url behind another url
 
 Usage:
 -----
@@ -9,6 +9,8 @@ Usage:
     
 """
 
+import sys
+import argparse
 from urllib.parse import urlparse
 
 from requests import post
@@ -40,10 +42,39 @@ def MaskUrl(target_url: str, mask_domain: str, keyword: str) -> str:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Mask the URL behind the another URL")
+
+    parser.add_argument(
+        "--target",
+        type=str,
+        help="Target URL to Mask (With http or https)",
+        required=True,
+    )
+    parser.add_argument(
+        "--mask",
+        type=str,
+        help="Mask URL (With http or https)",
+        required=True,
+    )
+    parser.add_argument(
+        "--keywords",
+        type=str,
+        help="Keywords (Use (-) instead of whitespace)",
+        required=True,
+    )
+
     print(f"\033[91m {banner}\033[00m")
-    print("\n")
-    target = input("Enter the url (With http or https): ")
-    domain = input("Enter the domain name to mask url (With http or https): ")
-    keyword = input("Enter the keywords (use '-' instead of whitespace): ")
-    print("\n")
-    print(f"\033[91m {MaskUrl(target, domain, keyword)}\033[00m")
+
+    if len(sys.argv) == 1:
+        print("\n")
+        target = input("Enter the url (With http or https): ")
+        mask = input("Enter the domain name to mask url (With http or https): ")
+        keyword = input("Enter the keywords (use '-' instead of whitespace): ")
+        print("\n")
+    else:
+        args = parser.parse_args()
+        target = args.target
+        mask = args.mask
+        keyword = args.keywords
+
+    print(f"\033[91m {MaskUrl(target, mask, keyword)}\033[00m")
